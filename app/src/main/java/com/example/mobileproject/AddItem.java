dfb8b6fa7c7ee9ddbdcbb49681cf475464bff07b
Akyas <akyas.s.official@gmail.com>
2025-07-05 20:09:10 +0800

    rest if the files

---
+++ app/src/main/java/com/example/mobileproject/AddItem.java
@@ -0,0 +1,50 @@
+package com.example.mobileproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AddItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_item);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.additem);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            } else if (id == R.id.wastelog) {
+                startActivity(new Intent(this, WasteLogActivity.class));
+                return true;
+            } else if (id == R.id.additem) {
+                // Already in AddItem
+                return true;
+            } else if (id == R.id.recepie) {
+                startActivity(new Intent(this, RecepieActivity.class));
+                return true;
+            } else if (id == R.id.profile) {
+                startActivity(new Intent(this, ProfileActivity.class));
+                return true;
+            }
+            return false;
+        });
+    }
+}
